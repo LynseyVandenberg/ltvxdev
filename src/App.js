bfa7d2aaae14d69a11c401383c70878003eaffd9
Lynsey Vandenberg <lynsey.vandenberg@gmail.com>
2021-09-18 15:24:15 -0400

    more browserrouter troubleshooting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Home from './components/Home/'
 import Footer from './components/Footer/'
 import About from './components/About'
@@ -6,29 +9,26 @@ import NavBar from './components/NavBar/'
 import Contact from './components/Contact'
 import Projects from './components/Projects'
 import Resume from './components/Resume'
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
-function App() {
-  return (
-    <Router> 
-    <div className="app">
-<Router basename='https://lynseyvandenberg.github.io/ltvxdev/'>
-   <NavBar />
-<Switch>
-  <Route exact path="/" component={Home} />
-  <Route exact path="/about" component={About} />
-  <Route exact path="/contact" component={Contact} />
-  <Route exact path="/resume" component={Resume} />
-  <Route exact path="/projects/" component={Projects} />
-</Switch>
-<Footer />
-</Router>
-    </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/resume" component={Resume} />
+          <Route exact path="/projects/" component={Projects} />
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+
+    );
+  }
 }
 
 export default App;
