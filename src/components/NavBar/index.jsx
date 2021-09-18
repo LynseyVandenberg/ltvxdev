3e5274a63e773707f91663c38645a39569e78100
Lynsey Vandenberg <lynsey.vandenberg@gmail.com>
2021-09-18 19:49:39 -0400

    nav tests

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,24 +1,22 @@
-// import "./style.css";
-import { Navbar, Container, NavDropdown, Nav } from 'react-bootstrap';
-
+import { Navbar, Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
-
-    return (
+  return (
 <Navbar bg="light" expand="lg">
-  <Container>
+  <div>
   <Navbar.Brand href="/">Lynsey Vandenberg</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-        <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link href="/about">Bio</Nav.Link>
-        <Nav.Link href="/projects">Projects</Nav.Link>
-        <Nav.Link href="/resume">Resume</Nav.Link>
-        <Nav.Link href="/contact">Contact</Nav.Link>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/resume">Projects</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/resume">Resume</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
       </Nav>
     </Navbar.Collapse>
-  </Container>
+  </div>
 </Navbar>
     )
 }
