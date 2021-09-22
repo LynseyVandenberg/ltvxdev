193a1c5a1bf0dd3f7f734e6102fef87762259a65
Lynsey Vandenberg <lynsey.vandenberg@gmail.com>
2021-09-22 19:28:40 -0400

    updating projects

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -4,7 +4,7 @@ const Projects = () => {
     return (
 
         <div className="project">
-            <a href="https://lynseyvandenberg.github.io/work_day_scheduler/"><img src={`${process.env.PUBLIC_URL}/0.jpg`} onClick="" /></a>
+            <a href="https://boiling-beach-80721.herokuapp.com/"><img src={`${process.env.PUBLIC_URL}/0.jpg`} onClick="" /></a>
             <a href="https://bryanjperez.github.io/group6random/"><img src={`${process.env.PUBLIC_URL}/1.jpg`} onClick="" /></a>
             <a href="https://lynseyvandenberg.github.io/horiseon-refactor/"><img src={`${process.env.PUBLIC_URL}/2.jpg`} onClick="" /></a>
             <a href="https://lynseyvandenberg.github.io/run-buddy/"><img src={`${process.env.PUBLIC_URL}/3.jpg`} onClick="" /></a>
